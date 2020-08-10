954cafc2e3c0527d33db7ef18ad283c055937fbd
adanbae-dev <mostbae.dev@gmail.com>
2020-08-10 20:24:19 +0900

    component 구조

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -10,7 +10,7 @@ import '@public/style.css';
 ReactDOM.render(
   <div className='app'>
     <h4>Welcome to React, Electron and Typescript</h4>
-    <p></p>
+    <p>hot reload hot hot</p>
   </div>,
   document.getElementById('app')
 );
